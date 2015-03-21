68b26ae354455a6514b675b5c5cccc087df69f3a
Gust <darksshades@hotmail.com>
2015-03-21 19:20:26 -0300

    Configure deploy database

---
+++ readme.md
@@ -0,0 +1,13 @@
+Uridiff Project
+
+
+=====
+Installing local
+
+Export the DATABASE_URL to appoint the local database
+
+Example, the following line in ~/.bashrc
+
+export DATABASE_URL=postgres:///uridiff
+
+Will use a database named uridiff when you migrate locally
--- uridiff/settings.py
+++ uridiff/settings.py
@@ -13,9 +13,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 's$7v))=xvk-9(8u-w7vpj2zg+12711jdttbpdszur3689($hb&'
 
@@ -53,15 +50,11 @@ ROOT_URLCONF = 'uridiff.urls'
 WSGI_APPLICATION = 'uridiff.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -81,3 +74,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()

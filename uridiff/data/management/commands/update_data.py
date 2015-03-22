ab72621fe3e25e7d50fb48700b79ae6b45183bfe
Gust <darksshades@hotmail.com>
2015-03-22 02:03:57 -0300

    Fix unicode error

--- uridiff/data/management/commands/update_data.py
+++ uridiff/data/management/commands/update_data.py
@@ -5,7 +5,8 @@ from uridiff.data.crawler import Crawler
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        pages = kwargs.get('pages') or int(args[0])
-        print pages
+        pages = kwargs.get('pages') or args[0]
+        pages = int(pages)
+
         crawler = Crawler()
         crawler.update_questions(pages)

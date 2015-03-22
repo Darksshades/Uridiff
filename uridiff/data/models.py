f0ff0a143b54b93c3c795cdd949e89f208679e7b
Alexandre Barbosa <alexandreab@live.com>
2015-03-22 01:51:30 -0300

    Url to questions in home

--- uridiff/data/models.py
+++ uridiff/data/models.py
@@ -8,6 +8,10 @@ class Question(models.Model):
     solved = models.CharField(max_length=10, blank=True)
     level = models.IntegerField(default=1)
 
+    def get_url(self):
+        url = "https://www.urionlinejudge.com.br/judge/pt/problems/view/{}".format(self.id)
+        return url
+
 
 class UriUser(models.Model):
     id = models.IntegerField(primary_key=True)
